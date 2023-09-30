a783e5399124f2dbe0e276170cc33cffe45ebdee
JohnHa0 <8759237+JohnHa0@users.noreply.github.com>
2023-09-30 21:08:55 +0800

    fix the bug when handling the continuing IMG_NOs, the extra information such as age and nation is ignored.

--- data_utl.py
+++ data_utl.py
@@ -23,58 +23,66 @@ nation_encoding = {
 # LABEL_TO_AGE_RANGES = ['0-18', '19-30', '31-40', '41-50', '51-60', '61-80', '81-100']
 
 
-
 pd_new = pd.DataFrame()
-cols_order = ['img_idx', 'age_label','age_cat','gender_label','ethnicity_label','gender','ethnicity','age']
+cols_order = ['img_idx', 'age_label', 'age_cat', 'gender_label', 'ethnicity_label', 'gender', 'ethnicity', 'age']
 
-
-
-# 读取Excel文件
+# 1. 加载Excel文件
 df = pd.read_excel("./data/data_fine.xlsx")
 
-# Generate file path
-def generate_file_names(folder, number_pattern):
-    if isinstance(number_pattern, str):
-        if '-' in number_pattern:
-            start, end = map(int, number_pattern.split('-'))
-            return [f"{folder}/IMG_{i}.jpg" for i in range(start, end + 1)]
-        else:
-            return [f"{folder}/IMG_{number_pattern}.jpg"]
-    return []
+# 2. 使用list收集扩展的行数据
+expanded_data = []
 
-all_paths = []
-folder_names = df.iloc[:, 0].values
-jpg_numbers = df.iloc[:, -1].values
-for folder, number_pattern in zip(folder_names, jpg_numbers):
-    all_paths.extend(generate_file_names(folder, number_pattern))
+# 3. 遍历原始dataframe的每一行
+for _, row in df.iterrows():
+    # 检查照片编号是否为空或缺失
+    if pd.isnull(row["照片编号"]) or row["照片编号"] == "":
+        continue  # 跳过该行
 
-# df_full_paths = pd.DataFrame({"完整路径": all_paths})
-pd_new['img_idx'] = all_paths
+    # 拆分照片编号
+    numbers = row["照片编号"].split("-")
+
+    # 如果有范围的编号，生成该范围内的所有编号
+    if len(numbers) == 2:
+        start, end = map(int, numbers)
+        for num in range(start, end + 1):
+            file_path = f"{row['文件夹名']}/IMG_{num}.jpg"
+            expanded_data.append({
+                "img_idx": file_path,
+                "age": row["年龄"],
+                "gender": row["性别"],
+                "ethnicity": row["民族"]
+            })
+    # 如果只有一个编号，直接使用该编号
+    else:
+        file_path = f"{row['文件夹名']}/IMG_{numbers[0]}.jpg"
+        expanded_data.append({
+            "img_idx": file_path,
+            "age": row["年龄"],
+            "gender": row["性别"],
+            "ethnicity": row["民族"]
+        })
+
+# 4. 将收集的数据转换为dataframe
+expanded_df = pd.DataFrame(expanded_data)
 
 # Labeled for gender and nationality
-pd_new['gender'] = df['性别']
-pd_new['ethnicity'] = df['民族']
-pd_new['age'] = df['年龄']
-pd_new['gender_label'] = df['性别'].map(gender_encoding)
-pd_new['ethnicity_label'] = df['民族'].map(lambda x: nation_encoding.get(x, 8))
+
+expanded_df['gender_label'] = expanded_df['gender'].map(gender_encoding)
+expanded_df['ethnicity_label'] = expanded_df['ethnicity'].map(lambda x: nation_encoding.get(x, 8))
 
 # labeled for ages
 # LABEL_TO_AGE_RANGES = ['0-18', '19-30', '31-40', '41-50', '51-60', '61-80', '81-100']
 bins = [18, 30, 40, 50, 60, 80, 100]
-age_ranges= ['18-30', '31-40', '41-50', '51-60', '61-80', '81-100']
+age_ranges = ['18-30', '31-40', '41-50', '51-60', '61-80', '81-100']
 # age_labels = ['0', '1', '2', '3', '4', '5','6']
 # 使用cut函数进行分段编码 Right false 左闭右开
-pd_new['age_cat'] = pd.cut(df['年龄'], bins=bins, labels=age_ranges, right=False)
+expanded_df['age_cat'] = pd.cut(expanded_df['age'], bins=bins, labels=age_ranges, right=False)
 
 # 为年龄段提供数字编码，可以再添加一个编码列
 age_labels = {label: idx for idx, label in enumerate(age_ranges)}
-pd_new['age_label'] = pd_new['age_cat'].map(age_labels)
-
-
+expanded_df['age_label'] = expanded_df['age_cat'].map(age_labels)
 
 # Reorder
-pd_new = pd_new[cols_order]
+expanded_df = expanded_df[cols_order]
 # Save files
-pd_new.to_excel("./data/data_with_labels.xlsx", index=False)
-
-
+expanded_df.to_excel("./data/data_with_labels_new.xlsx", index=False)
